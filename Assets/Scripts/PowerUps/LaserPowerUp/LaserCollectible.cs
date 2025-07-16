424a9685716266e7eb6c6dcde2b0e95533df26f1
ReKylee <ido5490@gmail.com>
2025-07-17 00:10:17 +0300

    Refactors

--- Assets/Scripts/PowerUps/LaserPowerUp/LaserCollectible.cs
+++
@@ -1,9 +0,0 @@
-using PowerUps._Base;
-
-namespace PowerUps.LaserPowerUp
-{
-    public class LaserCollectible : PowerUpCollectibleBase
-    {
-        public override IPowerUp CreatePowerUp() => new LaserPowerUp();
-    }
-}
