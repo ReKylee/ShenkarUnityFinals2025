092a19d54605b47e0b23bf58726c58d42073c016
ReKylee <ido5490@gmail.com>
2025-07-20 20:47:29 +0300

    Containerrr

--- Assets/Scripts/PowerUps/PowerUpContainer.cs
+++ Assets/Scripts/PowerUps/PowerUpContainer.cs
@@ -1,55 +1,59 @@
-﻿using System;
-using Health.Interfaces;
-using PowerUps._Base;
+﻿using Health.Interfaces;
 using UnityEngine;
 
 namespace PowerUps
 {
     public class PowerUpContainer : MonoBehaviour, IDamageable
     {
-        [SerializeField] private PowerUpCollectibleBase powerUpCollectible;
-        [SerializeField] private Vector2 launchVelocity;
-        [SerializeField] private Sprite whole;
-        [SerializeField] private Sprite broken;
-        private Rigidbody2D _rb;
-        private SpriteRenderer _sr;
-
-        private void Awake()
-        {
-            _rb = GetComponent<Rigidbody2D>();
-            _sr = GetComponent<SpriteRenderer>();
-            _sr.sprite = whole;
-        }
-        private void AttemptBreak()
-        {
-            _rb.linearVelocity = launchVelocity;
-            _sr.sprite = broken;
-
-        }
+        [SerializeField] private Sprite crackedSprite;
+        [SerializeField] private GameObject powerUpPrefab;
+        [SerializeField] private Vector2 launchVel = new(2, 2);
+        private SpriteRenderer _spriteRenderer;
+        private Rigidbody2D _rigidbody;
         public int CurrentHp { get; private set; } = 2;
         public int MaxHp { get; } = 2;
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("Player") && CurrentHp == MaxHp)
+            {
+                _rigidbody.linearVelocity = launchVel;
+                Damage(1);
+            }
+            else if (other.gameObject.layer == LayerMask.NameToLayer("Ground") && CurrentHp < MaxHp)
+            {
+                BreakOpen();
+            }
+        }
+        private void BreakOpen()
+        {
+            Instantiate(powerUpPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
         public void Damage(int amount)
         {
             CurrentHp -= 1;
-
-            if (CurrentHp <= 0)
+            switch (CurrentHp)
             {
-                CurrentHp = 0;
-                BreakContainer();
-            }
-            else
-            {
-                AttemptBreak();
+                case 1:
+                    Crack();
+                    break;
+                case <= 0:
+                    BreakOpen();
+                    break;
             }
         }
-        private void BreakContainer()
+
+        private void Crack()
         {
-            if (!powerUpCollectible)
-                return;
-
-            PowerUpCollectibleBase pu = Instantiate(powerUpCollectible, transform.position, Quaternion.identity);
-            Destroy(gameObject);
+            _spriteRenderer.sprite = crackedSprite;
 
         }
+
     }
 }
