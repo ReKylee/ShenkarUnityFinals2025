05873cf0154f5cf716ebd65681785a04d3f660c3
ReKylee <ido5490@gmail.com>
2025-08-04 15:44:36 +0300

    MORE FIXES

--- Assets/Scripts/Health/Damage/PeriodicBypassDamageDealer.cs
+++ Assets/Scripts/Health/Damage/PeriodicBypassDamageDealer.cs
@@ -12,11 +12,13 @@ namespace Health.Damage
     [DisallowMultipleComponent]
     public class PeriodicBypassDamageDealer : MonoBehaviour
     {
+        [Header("Damage Settings")]
         [SerializeField] private int damageAmount = 1;
         [SerializeField] private float interval = 3f;
         private IBypassableDamageable _bypassable;
         private Coroutine _damageRoutine;
         private IEventBus _eventBus;
+        private bool _isLevelCompleted;
 
         [Inject]
         public void Construct(IEventBus eventBus)
@@ -28,14 +30,30 @@ namespace Health.Damage
         {
             _bypassable = GetComponent<IBypassableDamageable>();
         }
+        
         private void OnEnable()
         {
             _eventBus?.Subscribe<LevelCompletedEvent>(OnLevelCompleted);
+            _isLevelCompleted = false;
             
             if (_bypassable != null)
                 _damageRoutine = StartCoroutine(DamageLoop());
         }
+        
         private void OnLevelCompleted(LevelCompletedEvent obj)
+        {
+            _isLevelCompleted = true;
+            StopDamageRoutine();
+        }
+
+        private void OnDisable()
+        {
+            _isLevelCompleted = true;
+            StopDamageRoutine();
+            _eventBus?.Unsubscribe<LevelCompletedEvent>(OnLevelCompleted);
+        }
+
+        private void StopDamageRoutine()
         {
             if (_damageRoutine != null)
             {
@@ -44,20 +62,15 @@ namespace Health.Damage
             }
         }
 
-        private void OnDisable()
-        {
-            if (_damageRoutine != null)
-                StopCoroutine(_damageRoutine);
-            _eventBus?.Unsubscribe<LevelCompletedEvent>(OnLevelCompleted);
-        }
-
         private IEnumerator DamageLoop()
         {
-            while (true)
+            while (_bypassable != null && !_isLevelCompleted)
             {
                 _bypassable.DamageBypass(damageAmount);
                 yield return new WaitForSeconds(interval);
             }
+            
+            _damageRoutine = null;
         }
     }
 }
